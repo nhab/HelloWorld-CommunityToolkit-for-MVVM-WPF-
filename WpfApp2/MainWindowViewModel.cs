aff98c1b10437551a1db875919475d2c57fa17bc
nhab <me@b.com>
2023-05-02 13:43:42 +0430

    Add project files.

---
+++ WpfApp2/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WpfApp2
+{
+    public  partial class MainWindowViewModel:ObservableObject
+    {
+        [ObservableProperty]
+       // [NotifyCanExecuteChangedFor(nameof(ClickCommand))]
+        private string? firstName = "kevin";
+
+        public ICommand ClickCommand { get; }
+        public MainWindowViewModel()
+        {
+            ClickCommand = new RelayCommand(OnClick,CanClick);
+        }
+        private bool CanClick() => FirstName == "kevin";
+
+       
+        private void OnClick()
+        {
+            FirstName = "Robert";
+        }
+    }
+}
